b4b51ad44f12446f324e31c2bd359e34282b57d1
wangzhengping <wangzhengping@kezaihui.com>
2023-06-09 14:53:41 +0800

    修复部分错误

--- lib/src/utils/index.dart
+++ lib/src/utils/index.dart
@@ -1,5 +1,7 @@
 export 'align_util.dart';
 export 'assert_check.dart';
 export 'log_util.dart';
+export 'dynamic_data_util.dart';
+export 'dynamic_text_util.dart';
 export 'color_util.dart';
 export 'uuid_util.dart';
